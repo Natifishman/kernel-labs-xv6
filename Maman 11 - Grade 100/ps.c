808ecb9e113548dc4fa7523d16c4bcc5bdb34d49
Natanel Fishman <98776245+Natifishman@users.noreply.github.com>
2025-06-04 17:47:23 +0300

    Add files via upload

---
+++ Maman 11 - Grade 100/ps.c
@@ -0,0 +1,13 @@
+// Name: Natanel Maor Fishman
+// ID: 	 318555646
+// This code is running when we type ps in xv6, it calls the cps108() command defined in the user mode
+
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
+
+int main(int argc, char *argv[]) {
+	cps146();
+	exit();
+}
---
+++ Maman 12 - Grade 100/ps.c
@@ -0,0 +1,18 @@
+/*
+ * Author: Natanel Maor Fishman
+ * ID: 318555646
+ * The program includes standard calls and headers for a function cps146() before exiting.
+ */
+
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
+
+int
+main(int argc, char *argv[])
+{
+    cps146();
+
+    exit(1);
+}
